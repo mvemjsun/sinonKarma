337f5acdd25a7c6c2e29ccb779d7bc927ab55100
PTeng <puneet.teng@sky.uk>
2020-02-27 13:54:07 +0000

    load

--- scripts/echoData.groovy
+++ scripts/echoData.groovy
@@ -9,4 +9,18 @@ def showParams(String params) {
 def returnValue() {
   return "Hello world"
 }
+
+def ids = 
+[
+  '17711467',
+  '7D089856',
+  'F4B8FFA8',
+  '2A190EB0',
+  '40F0D3A5',
+  'EF680D52',
+  'FFEAFA56',
+  '9A9F3E2A',
+  '3E421C22',
+  '27A35817'
+]
 return this;
